bf567dc90bc6708a92274175fca46d0ed85d6923
Matthew Manickakumar <extmmanickakumar@gnhearing.com>
2018-08-22 09:07:28 +0200

    Add project files.

---
+++ SpecRunTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecRunTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GNResound")]
+[assembly: AssemblyProduct("SpecRunTest")]
+[assembly: AssemblyCopyright("Copyright © GNResound 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcbc3742-de07-46ca-b1cb-4e440e506dcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpecRunTest/Steps/TestSpecRunSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecRunTest.Steps
+{
+    [Binding]
+    public class TestSpecRunSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Console.WriteLine("I have entered (.*) into the calculator");
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("call to when");
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Console.WriteLine("call to then");
+        }
+    }
+}
